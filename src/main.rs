dc055c270812c35260a0d7bdc1079693e5208c67
Rodrigo Mello <rodbove@gmail.com>
2021-11-28 22:26:39 -0300

    feat: add event listening to app

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-use kube::{Client, ResourceExt, api::{Api, ListParams}};
 use k8s_openapi::api::core::v1::Pod;
-use std::io::{stdout, stdin};
-use termion::raw::IntoRawMode;
-use termion::event::{Key, Event};
+use kube::{
+    api::{Api, ListParams},
+    Client, ResourceExt,
+};
+use std::io::{stdin, stdout};
+use termion::event::{Event, Key};
 use termion::input::TermRead;
-use tui::Terminal;
+use termion::raw::IntoRawMode;
 use tui::backend::TermionBackend;
-use tui::widgets::{Block, Borders, List, ListItem};
-use tui::layout::{Layout, Constraint, Direction};
+use tui::layout::{Constraint, Direction, Layout};
 use tui::style::{Color, Modifier, Style};
+use tui::widgets::{Block, Borders, List, ListItem};
+use tui::Terminal;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -26,43 +29,29 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut terminal = Terminal::new(backend)?;
 
     let stdin = stdin();
-
-    loop {
-    terminal.draw(|f| {
-        let chunks = Layout::default()
-            .direction(Direction::Horizontal)
-            .margin(1)
-            .constraints(
-                [
-                    Constraint::Percentage(20),
-                    Constraint::Percentage(80),
-                ].as_ref()
-            )
-            .split(f.size());
-
-
-        let list_items = items.clone();
-        let list = List::new(list_items)
-            .block(Block::default().title("List").borders(Borders::ALL))
-            .style(Style::default().fg(Color::White))
-            .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
-            .highlight_symbol(">>");
-
-        let block = Block::default()
-             .title("Resources")
-             .borders(Borders::ALL);
-        f.render_widget(block, chunks[0]);
-        f.render_widget(list, chunks[1]);
-    })?;
-
     for c in stdin.events() {
+        terminal.draw(|f| {
+            let chunks = Layout::default()
+                .direction(Direction::Horizontal)
+                .margin(1)
+                .constraints([Constraint::Percentage(20), Constraint::Percentage(80)].as_ref())
+                .split(f.size());
+
+            let list_items = items.clone();
+            let list = List::new(list_items)
+                .block(Block::default().title("List").borders(Borders::ALL))
+                .style(Style::default().fg(Color::White))
+                .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
+                .highlight_symbol(">>");
+
+            let block = Block::default().title("Resources").borders(Borders::ALL);
+            f.render_widget(block, chunks[0]);
+            f.render_widget(list, chunks[1]);
+        })?;
         let evt = c.unwrap();
-        match evt {
-            Event::Key(Key::Char('q')) => break,
-            _ => {}
+        if let Event::Key(Key::Char('q')) = evt {
+            break;
         }
-    }
     }
     Ok(())
 }
-
